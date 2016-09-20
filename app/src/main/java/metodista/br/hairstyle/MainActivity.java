e0d0afef280952b54bdf8a51690caaf2ebc558dd
mssdesigner22 <mssdesigner@gmail.com>
2016-09-20 20:10:12 -0300

    primeiro commit

---
+++ app/src/main/java/metodista/br/hairstyle/MainActivity.java
@@ -0,0 +1,13 @@
+package metodista.br.hairstyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
