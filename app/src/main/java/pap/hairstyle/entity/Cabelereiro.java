bf505ac35519187653db9dede158979dd0569a78
renancaster <renancaster@gmail.com>
2016-10-22 13:59:16 -0200

    commit de teste

---
+++ app/src/main/java/pap/hairstyle/entity/Cabelereiro.java
@@ -0,0 +1,19 @@
+package pap.hairstyle.entity;
+
+/**
+ * Created by Renan on 22/10/2016.
+ */
+
+public class Cabelereiro {
+    private String nomeCabelereiro;
+
+
+    public String getNomeCabelereiro() {
+        return nomeCabelereiro;
+    }
+
+    public void setNomeCabelereiro(String nomeCabelereiro) {
+        this.nomeCabelereiro = nomeCabelereiro;
+    }
+
+}
---
+++ app/src/main/java/pap/hairstyle/service/CabelereiroService.java
@@ -0,0 +1,9 @@
+package pap.hairstyle.service;
+
+/**
+ * Created by Renan on 22/10/2016.
+ */
+
+public class CabelereiroService {
+
+}
